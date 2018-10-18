4ca490740c2db1dba19d212784e948b8126cb53a
Unknown <supra4keanu@hotmail.com>
2018-10-19 04:37:11 +1000

    header setup

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset='utf-8' />
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+
+    <title>Hourly - ERMS</title>
+
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <link rel='stylesheet' type='text/css' href='css/min/globals.min.css'>
+    <link rel='stylesheet' type='text/css' href='css/min/index.min.css'>
+
+</head>
+<body>
+
+    <div id='grid-container'>
+    
+        <div id='grid-header'>
+
+            <object data='img/materialicon/baseline-alarm_on-24px.svg'></object>
+            <h3 id='header-heading'>HOURLY</h3>
+        
+            <h1 id='header-nav-news' class='nav'>NEWS</h1> 
+            <h1 id='header-nav-timeoff' class='nav'>TIME OFF</h1>
+            <h1 id='header-nav-roster' class='nav'>ROSTER</h1>
+            <h1 id='header-nav-employees' class='nav'>EMPLOYEES</h1>
+            <h1 id='header-nav-login' class='nav'>LOGIN</h1>
+
+        </div>
+
+        <div id='grid-content'>
+        
+        </div>
+
+    </div>
+
+</body>
+
+</html>
