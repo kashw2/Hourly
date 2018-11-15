fae8be9846a88777f6c448921f89f30992b10297
Unknown <supra4keanu@hotmail.com>
2018-11-15 16:32:14 +1000

    registrant account creation

---
+++ inc/actions/register.account.inc.php
@@ -0,0 +1,142 @@
+<?php
+
+ob_start();
+
+chdir('../../');
+
+require_once('mysql.php');
+require_once('inc/classes/errors.class.inc.php');
+
+session_start();
+
+$RegError = new RegistrationError;
+
+if(
+    isset($_POST['username'])
+&&  !empty($_POST['username'])
+&&  isset($_POST['password'])
+&&  !empty($_POST['password'])
+) {
+
+    if($RegError->checkAccount($conn, $_POST['username']) == $_POST['username']) {
+
+        $_SESSION['Error'] = "Error: Account already exists.";
+
+        header('Location: ../../setup.php?token=' . session_id());
+
+        die();
+
+    } else {
+            
+        $Statement = mysqli_prepare($conn, '
+        INSERT INTO
+        hourly.accounts (
+            hourly.accounts.id,
+            hourly.accounts.username,
+            hourly.accounts.password,
+            hourly.accounts.email,
+            hourly.accounts.company,
+            hourly.accounts.position
+        ) VALUES (
+            DEFAULT,
+            ?,
+            ?,
+            ?,
+            ?,
+            ?
+        )
+        ');
+
+        mysqli_stmt_bind_param($Statement,
+        'sssss',
+        $_POST['username'],
+        md5($_POST['password']),
+        $_SESSION['User']['Email'],
+        $_SESSION['User']['Company'],
+        $_SESSION['User']['Position']
+        );
+
+        mysqli_stmt_execute($Statement);
+
+        header('Location: /.../home.php');
+
+        die();
+
+    }
+
+} else {
+
+    if($RegError->checkAccount($conn, $_POST['username']) == $_POST['username']) {
+
+        $_SESSION['Error'] = "Error: Account already exists.";
+
+        header('Location: ../../setup.php?token=' . session_id());
+
+        die();
+
+    } else {
+
+        if(
+            isset($_POST['username'])
+        &&  !empty($_POST['username'])
+        ) {
+
+            if(
+                isset($_POST['autogen'])
+            &&  !empty($_POST['autogen'])
+            &&  $_POST['autogen'] == "on"
+                ) {
+
+                $Password = substr(hash("md5", time()), 0, 8);
+
+                $Statement = mysqli_prepare($conn, '
+                INSERT INTO
+                hourly.accounts (
+                    hourly.accounts.id,
+                    hourly.accounts.username,
+                    hourly.accounts.password,
+                    hourly.accounts.email,
+                    hourly.accounts.company,
+                    hourly.accounts.position
+                ) VALUES (
+                    DEFAULT,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?
+                )
+                ');
+
+                mysqli_stmt_bind_param($Statement,
+                'sssss',
+                $_POST['username'],
+                $Password,
+                $_SESSION['User']['Email'],
+                $_SESSION['User']['Company'],
+                $_SESSION['User']['Position']
+                );
+
+                mysqli_stmt_execute($Statement);
+
+                header('Location: home.php');
+
+                die();
+
+            }
+
+        } else {
+
+            $_SESSION['Error'] = "Error: Input fields empty";
+
+            header('Location: /.../setup.php?token=' . session_id());
+
+            die();
+
+        }
+
+    }
+
+}
+
+?>
