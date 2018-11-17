1ceb62892ba51190baad0fc82ff42dfa38be8a3b
Unknown <supra4keanu@hotmail.com>
2018-11-17 21:50:05 +1000

    implemented new company registrant checks

--- inc/actions/register.account.inc.php
+++ inc/actions/register.account.inc.php
@@ -37,54 +37,64 @@ if(
             die();
 
         } else {
-            
-            $Statement = mysqli_prepare($conn, '
-            INSERT INTO
-            hourly.accounts (
-                hourly.accounts.id,
-                hourly.accounts.username,
-                hourly.accounts.password,
-                hourly.accounts.email,
-                hourly.accounts.company,
-                hourly.accounts.position
-            ) VALUES (
-                DEFAULT,
-                ?,
-                ?,
-                ?,
-                ?,
-                ?
-            )
-            ');
 
-            mysqli_stmt_bind_param($Statement,
-            'sssss',
-            $_POST['username'],
-            md5($_POST['password']),
-            $_SESSION['User']['Email'],
-            $_SESSION['User']['Company'],
-            $_SESSION['User']['Position']
-            );
+            if($RegError->checkRegistrantCompany($conn, $_SESSION['User']['Company']) > 0) {
 
-            mysqli_stmt_execute($Statement);
-
-            if(mysqli_error($conn)) {
-
-                $_SESSION['Error'] = 'Error';
+                $_SESSION['Error'] = "Error: This company already has a registrant.";
 
                 header('Location: ../../setup.php?token=' . session_id());
 
-                die();
-
             } else {
 
-                header('Location: ../../home.php');
+                $Statement = mysqli_prepare($conn, '
+                INSERT INTO
+                hourly.accounts (
+                    hourly.accounts.id,
+                    hourly.accounts.username,
+                    hourly.accounts.password,
+                    hourly.accounts.email,
+                    hourly.accounts.company,
+                    hourly.accounts.position
+                ) VALUES (
+                    DEFAULT,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?
+                )
+                ');
 
-                unset($_SESSION['Email']);
+                mysqli_stmt_bind_param($Statement,
+                'sssss',
+                $_POST['username'],
+                md5($_POST['password']),
+                $_SESSION['User']['Email'],
+                $_SESSION['User']['Company'],
+                $_SESSION['User']['Position']
+                );
+
+                mysqli_stmt_execute($Statement);
+
+                if(mysqli_error($conn)) {
+
+                    $_SESSION['Error'] = 'Error';
+
+                    header('Location: ../../setup.php?token=' . session_id());
+
+                    die();
+
+                } else {
+
+                    header('Location: ../../home.php');
+
+                    unset($_SESSION['Email']);
+
+                }
+
+                die();
 
             }
-
-            die();
 
         }
 
@@ -121,57 +131,67 @@ if(
                     isset($_POST['autogen'])
                 &&  !empty($_POST['autogen'])
                 &&  $_POST['autogen'] == "on"
-                    ) {
+                ) {
 
-                    $Password = substr(hash("md5", time()), 0, 8);
+                    if($RegError->checkRegistrantCompany($conn, $_SESSION['User']['Company']) > 0) {
 
-                    $Statement = mysqli_prepare($conn, '
-                    INSERT INTO
-                    hourly.accounts (
-                        hourly.accounts.id,
-                        hourly.accounts.username,
-                        hourly.accounts.password,
-                        hourly.accounts.email,
-                        hourly.accounts.company,
-                        hourly.accounts.position
-                    ) VALUES (
-                        DEFAULT,
-                        ?,
-                        ?,
-                        ?,
-                        ?,
-                        ?
-                    )
-                    ');
-
-                    mysqli_stmt_bind_param($Statement,
-                    'sssss',
-                    $_POST['username'],
-                    $Password,
-                    $_SESSION['User']['Email'],
-                    $_SESSION['User']['Company'],
-                    $_SESSION['User']['Position']
-                    );
-
-                    mysqli_stmt_execute($Statement);
-
-                    if(mysqli_error($conn)) {
-
-                        $_SESSION['Error'] = 'Error';
+                        $_SESSION['Error'] = "Error: This company already has a registrant.";
 
                         header('Location: ../../setup.php?token=' . session_id());
 
-                        die();
-
                     } else {
 
-                        header('Location: ../../home.php');
+                        $Password = substr(hash("md5", time()), 0, 8);
 
-                        unset($_SESSION['Email']);
+                        $Statement = mysqli_prepare($conn, '
+                        INSERT INTO
+                        hourly.accounts (
+                            hourly.accounts.id,
+                            hourly.accounts.username,
+                            hourly.accounts.password,
+                            hourly.accounts.email,
+                            hourly.accounts.company,
+                            hourly.accounts.position
+                        ) VALUES (
+                            DEFAULT,
+                            ?,
+                            ?,
+                            ?,
+                            ?,
+                            ?
+                        )
+                        ');
+
+                        mysqli_stmt_bind_param($Statement,
+                        'sssss',
+                        $_POST['username'],
+                        $Password,
+                        $_SESSION['User']['Email'],
+                        $_SESSION['User']['Company'],
+                        $_SESSION['User']['Position']
+                        );
+
+                        mysqli_stmt_execute($Statement);
+
+                        if(mysqli_error($conn)) {
+
+                            $_SESSION['Error'] = 'Error';
+
+                            header('Location: ../../setup.php?token=' . session_id());
+
+                            die();
+
+                        } else {
+
+                            header('Location: ../../home.php');
+
+                            unset($_SESSION['Email']);
+
+                        }
+
+                        die();
 
                     }
-
-                    die();
 
                 }
 
