0205f9a4d6aa0d97c92840da462decb3dc056653
Unknown <supra4keanu@hotmail.com>
2018-11-21 07:20:43 +1000

    function depreciation and removal

--- inc/classes/auth.class.inc.php
+++ inc/classes/auth.class.inc.php
@@ -15,41 +15,6 @@ class Authentication {
         echo __CLASS__ . ' class destroyed.';
     }
 
-    public function checkTokenUser($Connection) {
-
-        $Statement = mysqli_prepare($Connection, '
-        SELECT
-        hourly.accounts.username
-        FROM hourly.accounts
-        WHERE hourly.accounts.id = (
-            SELECT
-            hourly.sessions.userid
-            FROM hourly.sessions
-            WHERE hourly.sessions.token = ?
-        );
-        ');
-
-        mysqli_stmt_bind_param($Statement,
-        's',
-        session_id()
-        );
-
-        mysqli_stmt_execute($Statement);
-
-        mysqli_stmt_bind_result($Statement, $Result);
-
-        mysqli_stmt_fetch($Statement);
-
-        if($Result == session_id()) {
-
-            $Statement->close();
-
-            header('Location: home.php');
-
-        } 
-
-    }
-
     public function checkToken($Connection) {
 
     $Statement = mysqli_prepare($Connection, '
